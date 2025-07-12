06d048fedaf1f2d11d99c1ffc54a6eda79fda15a
TechSimplifide <ry0805502@gmail.com>
2025-07-12 17:13:59 +0530

    improved theme toggle

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const hamburger = document.getElementById("hamburger");
-      const navLinks = document.getElementById("navLinks");
+const navLinks = document.getElementById("navLinks");
 
-      hamburger.addEventListener("click", () => {
-        document.querySelector("button").style.display = "none";
-        navLinks.classList.toggle("show");
-      });
+hamburger.addEventListener("click", () => {
+  document.querySelector("button").style.display = "none";
+  navLinks.classList.toggle("show");
+});
+
+let btn = document.querySelector(".theme-toggler");
+let bg = true;
+btn.addEventListener("click", () => {
+  if (bg == true) {
+    document.body.style.backgroundColor = "#6b6a6a";
+    btn.style.backgroundColor = "#fff"
+    document.querySelector("#toggel-img").src = "day-mode.png"
+    document.querySelector("#quotes").style.color = "#fff"
+    bg = false;
+  } else {
+    document.body.style.backgroundColor = "";
+    btn.style.backgroundColor = "#6b6a6a"
+    document.querySelector("#toggel-img").src = "night-mode.png"
+    document.querySelector("#quotes").style.color = "black"
+    bg = true;
+  }
+});
